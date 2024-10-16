6388d3b2abb058e0da459c45f5b4bbb7138ddc74
phoenix007 <156402075+kesavanblack@users.noreply.github.com>
2024-10-16 16:28:15 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import Home from './Components/Home';
-import About from './Components/About';
-import Skills from './components/Skills';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
-import Header from './Header';
+// import Home from './Components/Home';
+// import About from './Components/About';
+// import Skills from './components/Skills';
+// import Projects from './Components/Projects';
+// import Contact from './Components/Contact';
+// import Footer from './Components/Footer';
+// import Header from './Header';
 
 
 const App = () => {
