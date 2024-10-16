48d63cc53f345e7607efe68e89fa053b10f984f8
phoenix007 <156402075+kesavanblack@users.noreply.github.com>
2024-10-16 16:30:28 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 
 import React from 'react';
-// import Home from './Components/Home';
-// import About from './Components/About';
-// import Skills from './components/Skills';
-// import Projects from './Components/Projects';
-// import Contact from './Components/Contact';
-// import Footer from './Components/Footer';
-// import Header from './Header';
+import Home from './Components/Home';
+import About from './Components/About';
+import Skills from './components/Skills';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Header from './Header';
 
 
 const App = () => {
   return (
     <>
-   {/* <Header/>
+   <Header/>
   <Home/>
   <About/>
   <Skills/>
   <Projects/>
   <Contact/>
-  <Footer/> */}
+  <Footer/>
 
 <div><h1>hi..!</h1></div>
   </>
